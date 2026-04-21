04d5314d4876875ceacc69d035ed992bab12b451
Uatilla Viana Almeida <uatilaalmeida@proton.me>
2026-04-21 21:24:25 +0100

    Done ex03

--- Module_0/ex03/NULL_not_found.py
+++ Module_0/ex03/NULL_not_found.py
@@ -1,14 +1,17 @@
 def NULL_not_found(object: any) -> int:
-    data_type = type(object)
-    type_map = {
-        list: "List",
-        tuple: "Tuple",
-        set: "Set",
-        dict: "Dict",
-        str: f"{object} is in the kitchen",
-        None: "Nothing",
-        
-    }
-    type_name = type_map.get(data_type)
-    print(type_name, data_type, sep=" : ")
-    return (42)
+    match object:
+        case None:
+            type_name = "Nothing"
+        case float() if object != object:
+            type_name = "Cheese"
+        case bool() if object is False:
+            type_name = "Fake"
+        case int() if object == 0:
+            type_name = "Zero"
+        case str() if object == "":
+            type_name = "Empty"
+        case _:
+            print("Type not Found")
+            return 1
+    print(f"{type_name}: {object} {type(object)}")
+    return (0)
